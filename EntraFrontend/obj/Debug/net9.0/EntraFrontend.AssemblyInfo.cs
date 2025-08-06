24235d78b6ec29a0823b4c53f9456a6e05fbf072
Mayowa Oladimeji <dimejikassie@gmail.com>
2025-08-06 09:49:12 +0100

    ui

--- EntraFrontend/obj/Debug/net9.0/EntraFrontend.AssemblyInfo.cs
+++ EntraFrontend/obj/Debug/net9.0/EntraFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntraFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fce7f620a65c1666f266a1d5278cc472605c9da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b39e63523d749ae1f9efae83ce925e101c142d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntraFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntraFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EntraFrontend/obj/Release/net9.0/EntraFrontend.AssemblyInfo.cs
+++ EntraFrontend/obj/Release/net9.0/EntraFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntraFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80ab019575e9293377252f6abaf97136de0b646")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b39e63523d749ae1f9efae83ce925e101c142d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntraFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntraFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
